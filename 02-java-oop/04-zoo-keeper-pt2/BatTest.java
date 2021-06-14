406342478a77e309190c2e1d006610e39541237a
Fumie <funismo@gmail.com>
2021-06-13 19:50:54 -0700

    third commit

--- 02-java-oop/04-zoo-keeper-pt2/BatTest.java
+++ 02-java-oop/04-zoo-keeper-pt2/BatTest.java
@@ -2,7 +2,11 @@ public class BatTest{
     public static void main(String[] args){
         Bat batman =  new Bat();
         batman.fly();
+        batman.fly();
         batman.eatHumans();
+        batman.eatHumans();
+        batman.attackTown();
+        batman.attackTown();
         batman.attackTown();
         batman.displayEnergy();
         // System.out.println(energy);
