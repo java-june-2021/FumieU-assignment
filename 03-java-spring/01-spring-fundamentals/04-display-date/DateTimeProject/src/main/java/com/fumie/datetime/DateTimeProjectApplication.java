c2f31fb14afab9f31255b29815af7a21eb2c4913
Fumie <funismo@gmail.com>
2021-06-22 17:30:50 -0700

    second commit

---
+++ 03-java-spring/01-spring-fundamentals/04-display-date/DateTimeProject/src/main/java/com/fumie/datetime/DateTimeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fumie.datetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateTimeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateTimeProjectApplication.class, args);
+	}
+
+}
---
+++ 03-java-spring/01-spring-fundamentals/04-display-date/DateTimeProject/src/test/java/com/fumie/datetime/DateTimeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fumie.datetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateTimeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
